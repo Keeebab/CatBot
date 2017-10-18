0c80c0cf3ac63036289da06d8cc2fd6df92b65d5
Keeebab <32479416+Keeebab@users.noreply.github.com>
2017-10-18 18:13:05 +0200

    Added Join/Leave embed

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = class {
   constructor(client) {
     this.client = client;
@@ -5,5 +7,17 @@ module.exports = class {
   async execute(member) {
     const guild = member.guild;
     this.client.points.set(`${guild.id}-${member.id}`, { points: 200, level:1, user: member.id, guild: guild.id, daily: 1504120109 });
+    
+  const embedcolor = Math.floor(Math.random() * 0x999099);
+  const settings = this.client.settings.get(guild.id);
+  const channel  = member.guild.channels.find('name', settings.memberLogChannel);
+  const embed = new Discord.RichEmbed()
+    .setColor(embedcolor)
+    .setTimestamp()
+    .setThumbnail(member.user.displayAvatarURL)
+    .setTitle('Member Joined')
+	.addField(`Name: ${member.user.tag}`, (member.user.id))
+
+  return channel.send({embed});
   }
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -7,5 +7,17 @@ module.exports = class {
   async execute(member) {
     const guild = member.guild;
     this.client.points.delete(`${guild.id}-${member.id}`);
+    
+  const embedcolor = Math.floor(Math.random() * 0x999099);
+  const settings = this.client.settings.get(guild.id);
+  const channel  = member.guild.channels.find('name', settings.memberLogChannel);
+  const embed = new Discord.RichEmbed()
+    .setColor(embedcolor)
+    .setTimestamp()
+    .setThumbnail(member.user.displayAvatarURL)
+    .setTitle('Member Left')
+	  .addField(`Name: ${member.user.tag}`, (member.user.id))
+
+  return channel.send({embed});
   }
 };
